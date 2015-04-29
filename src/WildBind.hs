641c363e8befea13b28546c3bfffd2c972a9a7da
Toshio Ito <debug.ito@gmail.com>
2015-04-29 12:20:36 +0900

    start writing basic types.

--- src/WildBind.hs
+++ src/WildBind.hs
@@ -2,9 +2,31 @@
 -- Module: WildBind
 -- Description:
 -- Maintainer: Toshio Ito <debug.ito@gmail.com>
--- 
+--
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
 module WildBind (
   
 ) where
+
+import Data.Text (Text)
+import Data.Foldable (Foldable, forM_)
+
+class Input i
+
+class Input i => InputGenerator g i where
+  inputTo :: g -> (i -> IO ()) -> IO ()
+
+class Input i => WindBind w i where
+  bind :: w -> i -> Maybe (IO ())
+
+instance (Foldable f, Input i) => InputGenerator (f i) i where
+  inputTo = forM_
+
+
+-----
+
+data SampleInput = SIA | SIB | SIC deriving (Eq,Ord,Show)
+
+instance Input SampleInput
 
 
