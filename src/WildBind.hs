9c2d3021c07c7bde0822182bc3ad43602cfea1d8
Toshio Ito <debug.ito@gmail.com>
2015-04-29 17:57:31 +0900

    WildBind: write expose list and haddock.

--- src/WildBind.hs
+++ src/WildBind.hs
@@ -5,39 +5,59 @@
 --
 {-# LANGUAGE MultiParamTypeClasses #-}
 module WildBind (
-  
+  ActionDescription,
+  FrontState,
+  FrontInput,
+  FrontInputDevice(..),
+  FrontEvent(..),
+  FrontEventSource(..),
+  Action(..),
+  FrontDescriber(..),
+  Engine(..),
+  wildBind
 ) where
 
 import Data.Maybe (isJust)
 import Data.Text (Text)
 
+-- | The state of front-end side. It can change with no regard with WildBind.
 class FrontState s
+
+-- | The type of input symbols, like key buttons.
 class FrontInput i
 
+-- | Human-readable description of an action.
 type ActionDescription = Text
 
+-- | Something that knows about and controls the input device.
 class (FrontInput i) => FrontInputDevice f i where
   defaultActionDescription :: f -> i -> ActionDescription
   setGrab :: f -> (i -> Bool) -> IO ()
 
+-- | Event from the front-end.
 data FrontEvent s i = FEInput s i
                     | FEChange s
 
+-- | Something that brings stream of events from the front-end.
 class (FrontState s, FrontInput i) => FrontEventSource f s i where
   nextEvent :: f -> IO (FrontEvent s i)  -- should it be a streaming I/F, like conduit?
 
+-- | Action done by WildBind
 data Action a = Action {
-  actDescription :: ActionDescription,
-  actDo :: IO a
+  actDescription :: ActionDescription, -- ^ Human-readable description of the action.
+  actDo :: IO a -- ^ The actual job.
 }
 
+-- | Something that describes current bindings for the user.
 class (FrontInput i) => FrontDescriber f i where
   describeActions :: f -> (i -> ActionDescription) -> IO ()
 
+-- | WildBind back-end engine.
 newtype Engine s i = Engine {
-  bindingFor :: s -> i -> Maybe (Action (Engine s i))
+  bindingFor :: s -> i -> Maybe (Action (Engine s i)) -- ^ If 'Nothing', the input is not bound.
 }
 
+-- | Combines the front-end and the 'Engine' and returns the executable.
 wildBind :: (FrontInputDevice f i, FrontEventSource f s i) => f -> Engine s i -> IO ()
 wildBind front engine = do
   event <- nextEvent front
