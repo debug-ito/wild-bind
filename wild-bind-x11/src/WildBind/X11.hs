54308d4c4543cd6146bfca31cffb6e687d23535d
Toshio Ito <debug.ito@gmail.com>
2015-04-29 21:52:21 +0900

    WildBind.X11: declare some types.

--- wild-bind-x11/src/WildBind/X11.hs
+++ wild-bind-x11/src/WildBind/X11.hs
@@ -1,10 +1,36 @@
 -- |
 -- Module: WildBind.X11
--- Description:
+-- Description: X11-specific implementation for WildBind
 -- Maintainer: Toshio Ito <debug.ito@gmail.com>
--- 
-module WildBind.X11 (
-  
-) where
+--
+-- Maybe this should be released as a separate package.
+{-# LANGUAGE MultiParamTypeClasses #-}
+module WildBind.X11 () where
 
+import Control.Applicative ((<$>))
 
+import Graphics.X11.Xlib (Display, openDisplay)
+import Data.Text (Text)
+
+import WildBind (FrontState, FrontInputDevice(..), FrontEventSource(..))
+import WildBind.NumPad (NumPadUnlockedInput(..))
+
+data ActiveWindow = ActiveWindow {
+  awName :: Text,
+  awDesc :: Text
+} deriving (Eq,Ord,Show)
+instance FrontState ActiveWindow
+
+data X11Handle = X11Handle {
+  x11Display :: Display
+}
+
+initX11Handle :: IO X11Handle
+initX11Handle = X11Handle <$> openDisplay ""
+
+instance FrontInputDevice X11Handle NumPadUnlockedInput where
+  defaultActionDescription = undefined
+  setGrab = undefined
+
+instance FrontEventSource X11Handle ActiveWindow NumPadUnlockedInput where
+  nextEvent = undefined
