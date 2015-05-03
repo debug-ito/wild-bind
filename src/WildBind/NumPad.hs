f19807370c606295009e347aac251caff1091033
Toshio Ito <debug.ito@gmail.com>
2015-05-03 17:29:14 +0900

    WildBind.NumPad: add descriptionForUnlocked

--- src/WildBind/NumPad.hs
+++ src/WildBind/NumPad.hs
@@ -3,10 +3,13 @@
 -- Description: Types about number pads
 -- Maintainer: Toshio Ito <debug.ito@gmail.com>
 --
+{-# LANGUAGE OverloadedStrings #-}
+module WildBind.NumPad (
+  NumPadUnlockedInput(..),
+  descriptionForUnlocked
+) where
 
-module WildBind.NumPad (NumPadUnlockedInput(..)) where
-
-import WildBind (FrontInput)
+import WildBind (FrontInput, ActionDescription)
 
 -- | Number pad key input with NumLock disabled.
 data NumPadUnlockedInput
@@ -29,3 +32,23 @@ data NumPadUnlockedInput
   deriving (Eq,Ord,Show,Bounded,Enum)
 
 instance FrontInput NumPadUnlockedInput
+
+-- | default description for unlocked numpad keys
+descriptionForUnlocked :: NumPadUnlockedInput -> ActionDescription
+descriptionForUnlocked input = case input of
+  NumHome -> "Home"
+  NumUp -> "↑"
+  NumPageUp -> "PageUp"
+  NumLeft -> "←"
+  NumCenter -> ""
+  NumRight -> "→"
+  NumEnd -> "End"
+  NumDown -> "↓"
+  NumPageDown -> "PageDown"
+  NumDivide -> "/"
+  NumMulti -> "*"
+  NumMinus -> "-"
+  NumPlus -> "+"
+  NumEnter -> "Enter"
+  NumInsert -> "Insert"
+  NumDelete -> "Delete"
