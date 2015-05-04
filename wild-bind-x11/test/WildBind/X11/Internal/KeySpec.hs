f57d89812267335a036b5f1a9f733dbcbf7b076d
Toshio Ito <debug.ito@gmail.com>
2015-05-04 11:44:15 +0900

    X11.Internal.KeySpec: use newtype to get rid of orphan instance.

--- wild-bind-x11/test/WildBind/X11/Internal/KeySpec.hs
+++ wild-bind-x11/test/WildBind/X11/Internal/KeySpec.hs
@@ -6,13 +6,17 @@ import Test.QuickCheck (Arbitrary(arbitrary), arbitraryBoundedEnum, property)
 import WildBind.NumPad (NumPadUnlockedInput)
 import WildBind.X11.Internal.Key (KeySymLike(fromKeySym,toKeySym))
 
-instance Arbitrary NumPadUnlockedInput where
-  arbitrary = arbitraryBoundedEnum
+newtype NumPadUnlockedInput' = NumPadUnlockedInput' {
+  unwrapNumPadUnlockedInput :: NumPadUnlockedInput -- maybe we should use Control.Newtype
+} deriving Show
+
+instance Arbitrary NumPadUnlockedInput' where
+  arbitrary = fmap NumPadUnlockedInput' arbitraryBoundedEnum
 
 spec :: Spec
 spec = do
   describe "NumPadUnlockedInput" $ do
-    it "is a KeySymLike" $ property $ (prop_keySymLike :: NumPadUnlockedInput -> Bool)
+    it "is a KeySymLike" $ property $ (prop_keySymLike . unwrapNumPadUnlockedInput)
 
 prop_keySymLike :: (KeySymLike k, Eq k) => k -> Bool
 prop_keySymLike key = (fromKeySym . toKeySym) key == Just key
