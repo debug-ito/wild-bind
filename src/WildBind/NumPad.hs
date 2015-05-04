de30bb5a6926e92afda4ef90510813c1a6f6e845
Toshio Ito <debug.ito@gmail.com>
2015-05-04 17:07:07 +0900

    NumPad: add NumPadLockedInput type. NumPadUnlockedInput data constructors are re-arranged so that it matches with those of NumPadLockedInput.

--- src/WildBind/NumPad.hs
+++ src/WildBind/NumPad.hs
@@ -3,31 +3,36 @@
 -- Description: Types about number pads
 -- Maintainer: Toshio Ito <debug.ito@gmail.com>
 --
+-- Input types for number pad keys.
 {-# LANGUAGE OverloadedStrings #-}
 module WildBind.NumPad (
+  -- * NumLock disabled
   NumPadUnlockedInput(..),
-  descriptionForUnlocked
+  descriptionForUnlocked,
+  -- * NumLock enabled
+  NumPadLockedInput(..),
+  descriptionForLocked
 ) where
 
 import WildBind (FrontInput, ActionDescription)
 
 -- | Number pad key input with NumLock disabled.
 data NumPadUnlockedInput
-  = NumHome
-  | NumUp
-  | NumPageUp
-  | NumLeft
-  | NumCenter
-  | NumRight
+  = NumInsert
   | NumEnd
   | NumDown
   | NumPageDown
+  | NumLeft
+  | NumCenter
+  | NumRight
+  | NumHome
+  | NumUp
+  | NumPageUp
   | NumDivide
   | NumMulti
   | NumMinus
   | NumPlus
   | NumEnter
-  | NumInsert
   | NumDelete
   deriving (Eq,Ord,Show,Bounded,Enum)
 
@@ -52,3 +57,48 @@ descriptionForUnlocked input = case input of
   NumEnter -> "Enter"
   NumInsert -> "Insert"
   NumDelete -> "Delete"
+
+
+-- | Number pad key input with NumLock enabled.
+data NumPadLockedInput
+  = NumL0
+  | NumL1
+  | NumL2
+  | NumL3
+  | NumL4
+  | NumL5
+  | NumL6
+  | NumL7
+  | NumL8
+  | NumL9
+  | NumLDivide
+  | NumLMulti
+  | NumLMinus
+  | NumLPlus
+  | NumLEnter
+  | NumLPeriod
+  deriving (Eq,Ord,Show,Bounded,Enum)
+
+instance FrontInput NumPadLockedInput
+
+-- | default description for locked numpad keys
+descriptionForLocked :: NumPadLockedInput -> ActionDescription
+descriptionForLocked input = case input of
+  NumL0 -> "0"
+  NumL1 -> "1"
+  NumL2 -> "2"
+  NumL3 -> "3"
+  NumL4 -> "4"
+  NumL5 -> "5"
+  NumL6 -> "6"
+  NumL7 -> "7"
+  NumL8 -> "8"
+  NumL9 -> "9"
+  NumLDivide -> "/"
+  NumLMulti -> "*"
+  NumLMinus -> "-"
+  NumLPlus -> "+"
+  NumLEnter -> "Enter"
+  NumLPeriod -> "."
+
+
