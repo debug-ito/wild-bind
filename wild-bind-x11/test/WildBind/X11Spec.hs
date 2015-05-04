8473de49453b4d3f3dac29b599c8232b6110f041
Toshio Ito <debug.ito@gmail.com>
2015-05-04 12:18:29 +0900

    WildBind.X11Spec: implement preliminary test for key grabbing.

--- wild-bind-x11/test/WildBind/X11Spec.hs
+++ wild-bind-x11/test/WildBind/X11Spec.hs
@@ -1,12 +1,27 @@
 module WildBind.X11Spec (main, spec) where
 
+import System.IO (hPutStrLn,stderr)
 import Test.Hspec
+
+import WildBind (setGrab,unsetGrab,nextEvent,FrontEvent)
+import qualified WildBind.NumPad as NumPad
+import WildBind.X11 (initX11Front,ActiveWindow,X11Front)
 
 main :: IO ()
 main = hspec spec
 
+p :: String -> IO ()
+p = hPutStrLn stderr . ("--- " ++)
+
+nextEvent' :: X11Front -> IO (FrontEvent ActiveWindow NumPad.NumPadUnlockedInput)
+nextEvent' = nextEvent
+
 spec :: Spec
 spec = do
-  describe "someFunction" $ do
-    it "should work fine" $ do
-      True `shouldBe` False
+  describe "X11Front" $ do
+    it "should grab/ungrab keys" $ do
+      f <- initX11Front
+      setGrab f NumPad.NumMulti
+      p "Press some numpad keys> "
+      ev <- nextEvent' f
+      p $ show ev
