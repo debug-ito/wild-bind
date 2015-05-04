203760f5b4cf37f6e5bddaf120e70ee1744a805a
Toshio Ito <debug.ito@gmail.com>
2015-05-04 12:45:45 +0900

    X11Spec: run test conditionally on environment variables.

--- wild-bind-x11/test/WildBind/X11Spec.hs
+++ wild-bind-x11/test/WildBind/X11Spec.hs
@@ -1,6 +1,8 @@
 module WildBind.X11Spec (main, spec) where
 
 import System.IO (hPutStrLn,stderr)
+import System.Environment (lookupEnv)
+import Control.Applicative ((<$>))
 import Test.Hspec
 
 import WildBind (setGrab,unsetGrab,nextEvent,FrontEvent)
@@ -9,6 +11,20 @@ import WildBind.X11 (initX11Front,ActiveWindow,X11Front)
 
 main :: IO ()
 main = hspec spec
+
+checkEnv :: String -> IO Bool
+checkEnv env_name = (== "1") <$> maybe "" id <$> lookupEnv env_name
+
+whenEnv :: String -> Expectation -> Expectation
+whenEnv env_name exp = do
+  ret <- checkEnv env_name
+  if ret
+  then exp
+  else pendingWith ("You need to set " ++ env_name ++ "=1 to run the test")
+
+
+whenNumPad :: Expectation -> Expectation
+whenNumPad = whenEnv "WILDBIND_TEST_NUMPAD"
 
 p :: String -> IO ()
 p = hPutStrLn stderr . ("--- " ++)
@@ -19,9 +35,10 @@ nextEvent' = nextEvent
 spec :: Spec
 spec = do
   describe "X11Front" $ do
-    it "should grab/ungrab keys" $ do
+    it "should grab/ungrab keys" $ whenNumPad $ do
       f <- initX11Front
       setGrab f NumPad.NumMulti
       p "Press some numpad keys> "
       ev <- nextEvent' f
       p $ show ev
+
