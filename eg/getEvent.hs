3987fe91c1f2ccbe2118c855ca250af8dbdc33c8
Toshio Ito <debug.ito@gmail.com>
2015-07-26 10:14:29 +0900

    eg/getEvent.hs: try grabbing NumCenter. Worked.

--- eg/getEvent.hs
+++ eg/getEvent.hs
@@ -1,14 +1,17 @@
 module Main (main) where
 
-import WildBind (FrontEventSource(nextEvent), FrontEvent)
-import WildBind.NumPad (NumPadUnlockedInput)
+import WildBind (FrontEventSource(nextEvent), FrontEvent, setGrab)
+import WildBind.NumPad (NumPadUnlockedInput(NumCenter))
 import WildBind.X11 (withX11Front, ActiveWindow)
 
 main :: IO ()
-main = withX11Front $ \x11 -> loop x11 where
-  loop x11 = do
-    printEvent =<< nextEvent x11
-    loop x11
+main = withX11Front $ \x11 -> do
+  setGrab x11 NumCenter
+  loop x11
+    where
+      loop x11 = do
+        printEvent =<< nextEvent x11
+        loop x11
 
 printEvent :: FrontEvent ActiveWindow NumPadUnlockedInput -> IO ()
 printEvent = print
