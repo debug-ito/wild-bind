a6993663972213c8afda68553171659e6c893ec5
Toshio Ito <debug.ito@gmail.com>
2015-08-17 21:50:56 +0900

    BindingSpec: declare Arbitrary instances for SampleState and SampleInput.

--- test/WildBind/BindingSpec.hs
+++ test/WildBind/BindingSpec.hs
@@ -5,9 +5,9 @@ import Data.Monoid (mempty)
 import Data.Maybe (isNothing)
 
 import Test.Hspec
-import Test.QuickCheck (Gen, arbitraryBoundedEnum, arbitrary, property)
+import Test.QuickCheck (Gen, Arbitrary(arbitrary,shrink), arbitraryBoundedEnum, property)
 
-import WildBind.Binding(Binding, boundAction)
+import qualified WildBind.Binding as WB
 
 main :: IO ()
 main = hspec spec
@@ -15,15 +15,25 @@ main = hspec spec
 data SampleInput = SIa | SIb | SIc
                  deriving (Show, Eq, Ord, Enum, Bounded)
 
+instance Arbitrary SampleInput where
+  arbitrary = arbitraryBoundedEnum
+
 data SampleState = SS String
                  deriving (Show, Eq, Ord)
 
-genSamples :: Gen (SampleState, SampleInput)
-genSamples = (,) <$> (SS <$> arbitrary) <*> arbitraryBoundedEnum
+instance Arbitrary SampleState where
+  arbitrary = SS <$> arbitrary
+  shrink (SS s) = SS <$> shrink s
+
+-- genStatelessBinding :: Arbitrary a => IORef [a] -> Gen (WB.Binding s SampleInput)
+-- genStatelessBinding = do
+--   WB.on' <$> arbitraryBoundedEnum
+
+mempty_stateless :: WB.Binding SampleState SampleInput
+mempty_stateless = mempty
 
 spec :: Spec
 spec = do
   describe "Binding (Monoid instances)" $ do
-    it "mempty returns empty binding" $ property $ do
-      (s, i) <- genSamples
-      return (isNothing $ boundAction mempty s i)
+    it "mempty returns empty binding" $ property
+      ( isNothing <$> (WB.boundAction mempty_stateless <$> arbitrary <*> arbitrary) )
