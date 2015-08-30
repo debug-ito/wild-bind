c5ab504d31aaf645f7d28711404239a06eb49bb5
Toshio Ito <debug.ito@gmail.com>
2015-08-30 18:08:11 +0900

    WildBindSpec: prepare mock 'FrontInputDevice' and 'FrontEventSource'.

--- test/WildBindSpec.hs
+++ test/WildBindSpec.hs
@@ -1,6 +1,24 @@
 module WildBindSpec (main, spec) where
 
 import Test.Hspec
+import Control.Concurrent (Chan, readChan)
+import Data.IORef (IORef, modifyIORef)
+
+import qualified WildBind as WB
+
+newtype EventChan s i = EventChan { unEventChan :: Chan (WB.FrontEvent s i) }
+
+instance WB.FrontEventSource (EventChan s i) s i where
+  nextEvent = readChan . unEventChan
+
+data GrabHistory i = GSet i | GUnset i
+
+newtype GrabRef i = GrabRef { unGrabRef :: IORef [GrabHistory i] }
+
+instance WB.FrontInputDevice (GrabRef i) i where
+  defaultActionDescription _ _ = ""
+  setGrab f i = modifyIORef (unGrabRef f) (GSet i :)
+  unsetGrab f i = modifyIORef (unGrabRef f) (GUnset i :)
 
 main :: IO ()
 main = hspec spec
