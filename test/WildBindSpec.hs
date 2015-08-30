423bc2133f74796f37f8a8911b43bb7c961de795
Toshio Ito <debug.ito@gmail.com>
2015-08-30 21:55:18 +0900

    WildBindSpec: Use STM modules. they are gonna be needed anyway...

--- test/WildBindSpec.hs
+++ test/WildBindSpec.hs
@@ -1,24 +1,23 @@
 module WildBindSpec (main, spec) where
 
 import Test.Hspec
-import Control.Concurrent (Chan, readChan)
-import Data.IORef (IORef, modifyIORef)
+import Control.Concurrent.STM (atomically, TChan, readTChan, writeTChan)
 
 import qualified WildBind as WB
 
-newtype EventChan s i = EventChan { unEventChan :: Chan (WB.FrontEvent s i) }
+newtype EventChan s i = EventChan { unEventChan :: TChan (WB.FrontEvent s i) }
 
 instance WB.FrontEventSource (EventChan s i) s i where
-  nextEvent = readChan . unEventChan
+  nextEvent = atomically . readTChan . unEventChan
 
 data GrabHistory i = GSet i | GUnset i
 
-newtype GrabRef i = GrabRef { unGrabRef :: IORef [GrabHistory i] }
+newtype GrabChan i = GrabChan { unGrabChan :: TChan (GrabHistory i) }
 
-instance WB.FrontInputDevice (GrabRef i) i where
+instance WB.FrontInputDevice (GrabChan i) i where
   defaultActionDescription _ _ = ""
-  setGrab f i = modifyIORef (unGrabRef f) (GSet i :)
-  unsetGrab f i = modifyIORef (unGrabRef f) (GUnset i :)
+  setGrab f i = atomically $ writeTChan (unGrabChan f) (GSet i)
+  unsetGrab f i = atomically $ writeTChan (unGrabChan f) (GUnset i)
 
 main :: IO ()
 main = hspec spec
