37f80862729fd4b3ebe8e129d604857338ae7803
Toshio Ito <debug.ito@gmail.com>
2015-08-31 21:32:16 +0900

    WildBindSpec: start writing a test for 'wildBind'.

--- test/WildBindSpec.hs
+++ test/WildBindSpec.hs
@@ -1,9 +1,15 @@
 module WildBindSpec (main, spec) where
 
 import Test.Hspec
-import Control.Concurrent.STM (atomically, TChan, readTChan, writeTChan)
+import Control.Applicative ((<$>))
+import Control.Exception (bracket)
+import Control.Concurrent (forkIO, killThread)
+import Control.Concurrent.STM (atomically, TChan, readTChan, writeTChan, newTChanIO)
+import Control.Monad.IO.Class (MonadIO, liftIO)
 
 import qualified WildBind as WB
+import qualified WildBind.Binding as WBB
+import WildBind.ForTest (SampleInput(..), SampleState(..))
 
 newtype EventChan s i = EventChan { unEventChan :: TChan (WB.FrontEvent s i) }
 
@@ -19,11 +25,28 @@ instance WB.FrontInputDevice (GrabChan i) i where
   setGrab f i = atomically $ writeTChan (unGrabChan f) (GSet i)
   unsetGrab f i = atomically $ writeTChan (unGrabChan f) (GUnset i)
 
+
+outChanOn :: MonadIO m => TChan a -> i -> a -> (i, WBB.Action m ())
+outChanOn out_chan input out_elem = WBB.on' input "" (liftIO $ atomically $ writeTChan out_chan out_elem)
+
+withWildBind :: Ord i => WBB.Binding s i -> (EventChan s i -> GrabChan i -> IO ()) -> IO ()
+withWildBind binding action = do
+  echan <- EventChan <$> newTChanIO
+  gchan <- GrabChan <$> newTChanIO
+  let spawnWildBind = forkIO $ WB.wildBind gchan echan binding
+  bracket spawnWildBind killThread (\_ -> action echan gchan)
+  
+
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
   describe "wildBind" $ do
-    it "TODO: check wildBind's expected behavior, like, updating grab-sets according to Binding." $ do
-      True `shouldBe` False
+    it "should enable input grabs" $ do
+      ochan <- newTChanIO
+      let b = WBB.stateless [outChanOn ochan SIa 'A',
+                             outChanOn ochan SIb 'B']
+      withWildBind b $ \(EventChan echan) (GrabChan gchan) -> do
+        undefined
+      
