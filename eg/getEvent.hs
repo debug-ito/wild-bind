759b07b7efaac69382015edf2060ddbc2314a066
Toshio Ito <debug.ito@gmail.com>
2015-09-20 21:19:23 +0900

    eg/getEvent: adapt to the latest API.

--- eg/getEvent.hs
+++ eg/getEvent.hs
@@ -1,16 +1,16 @@
 module Main (main) where
 
-import WildBind (FrontEventSource(nextEvent), FrontEvent, setGrab)
-import WildBind.NumPad (NumPadUnlockedInput(NumCenter))
+import WildBind (FrontEnd(frontSetGrab, frontNextEvent), FrontEvent)
+import WildBind.Input.NumPad (NumPadUnlockedInput(NumCenter))
 import WildBind.X11 (withX11Front, ActiveWindow)
 
 main :: IO ()
 main = withX11Front $ \x11 -> do
-  setGrab x11 NumCenter
+  frontSetGrab x11 NumCenter
   loop x11
     where
       loop x11 = do
-        printEvent =<< nextEvent x11
+        printEvent =<< frontNextEvent x11
         loop x11
 
 printEvent :: FrontEvent ActiveWindow NumPadUnlockedInput -> IO ()
