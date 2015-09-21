2839f76bf486452c1e981369d7b5c02663a015a2
Toshio Ito <debug.ito@gmail.com>
2015-09-21 19:08:27 +0900

    Indicator: add basic data types and function signatures.

--- src/WildBind/Indicator.hs
+++ src/WildBind/Indicator.hs
@@ -4,7 +4,32 @@
 -- Maintainer: Toshio Ito <debug.ito@gmail.com>
 -- 
 module WildBind.Indicator (
-  
+  withIndicator,
+  Indicator,
+  updateDescription,
+  getPresence,
+  setPresence,
+  togglePresence
 ) where
 
+import WildBind (ActionDescription)
 
+-- | Indicator interface. @s@ is the front-end state, @i@ is the input
+-- type.
+data Indicator s i = Indicator {
+  updateDescription :: [(i, ActionDescription)] -> IO (),
+  -- ^ Update and show the description for the current binding.
+  getPresence :: IO Bool,
+  -- ^ Get the current presence of the indicator. Returns 'True' if
+  -- it's present.
+  setPresence :: Bool -> IO ()
+  -- ^ Set the presence of the indicator.
+  }
+
+-- | Toggle the presence of the indicator.
+togglePresence :: Indicator s i -> IO ()
+togglePresence ind = (setPresence ind . not) =<< getPresence ind
+
+-- | Initialize the indicator and run the given action.
+withIndicator :: (Indicator s i -> IO a) -> IO a
+withIndicator = undefined
