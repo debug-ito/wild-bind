b838f558c7c134fc3c457cc17109761ae7e8aae6
Toshio Ito <debug.ito@gmail.com>
2016-08-25 21:28:31 +0900

    ReadmeTest: wrote skeleton

--- test-wild-bind-doc/ReadmeTest.hs
+++ test-wild-bind-doc/ReadmeTest.hs
@@ -1,12 +1,43 @@
-module Main (main,spec) where
+module Main (main) where
 
+import Control.Applicative ((<$>))
 import Test.Hspec
 
 main :: IO ()
-main = hspec spec
+main = do
+  specs <- map specFor <$> makeTestCases <$> extractExamples <$> loadREADME
+  hspec $ sequence_ specs
 
-spec :: Spec
-spec = describe "README.md" $ do
-  it "should fail for now" $ do
-    True `shouldBe` False
+specFor :: TestCase -> Spec
+specFor tc = describe label $ do
+  it "should compile OK" $ checkCompile tc
+  where
+    label = "example " ++ (show $ tcIndex tc)
 
+loadREADME :: IO String
+loadREADME = undefined
+
+
+type CodeBlock = String
+
+data TestCase = TestCase { tcIndex :: Int,
+                           tcPrefix :: CodeBlock,
+                           tcBody :: CodeBlock
+                         } deriving (Show,Eq,Ord)
+
+extractExamples :: String -> [CodeBlock]
+extractExamples = undefined
+
+prefixFor :: Int -> CodeBlock
+prefixFor = undefined
+
+makeTestCases :: [CodeBlock] -> [TestCase]
+makeTestCases = map f . zip [0 ..] where
+  f (i, cb) = TestCase { tcIndex = i,
+                         tcPrefix = prefixFor i,
+                         tcBody = cb
+                       }
+
+
+checkCompile :: TestCase -> Expectation
+checkCompile = undefined
