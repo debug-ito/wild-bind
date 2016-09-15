a9837da843770d0767fd173d06a64c6f303b5ac7
Toshio Ito <debug.ito@gmail.com>
2016-09-15 22:30:54 +0900

    x11: use lookupEnv to check test runnability
    getEnv throws an exception when the environment variable doesn't
    exist. It sucks.

--- wild-bind-x11/test/WildBind/X11/TestUtil.hs
+++ wild-bind-x11/test/WildBind/X11/TestUtil.hs
@@ -10,11 +10,13 @@ module WildBind.X11.TestUtil
 
 import Control.Monad (when)
 import Control.Applicative ((<$>))
-import System.Environment (getEnv)
+import System.Environment (lookupEnv)
 import Test.Hspec
 
 isDisplaySet :: IO Bool
-isDisplaySet = (/= "") <$> getEnv "DISPLAY"
+isDisplaySet = exists <$> lookupEnv "DISPLAY" where
+  exists (Just v) = v /= ""
+  exists _ = False
 
 checkIfX11Available :: Spec -> Spec
 checkIfX11Available = before $ do
