95a1c3b4e5dc0beb0ec5a23c67c4ba59b86a5b30
Toshio Ito <debug.ito@gmail.com>
2017-11-27 21:45:57 +0900

    Emulate: add skeleton of some functions.

--- wild-bind-x11/src/WildBind/X11/Emulate.hs
+++ wild-bind-x11/src/WildBind/X11/Emulate.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 -- |
 -- Module: WildBind.X11.Emulate
 -- Description: X11 event emulation functions
@@ -5,21 +6,55 @@
 --
 -- 
 module WildBind.X11.Emulate
-       ( sendKeyEventTo
+       ( sendKeyEventTo,
+         sendKeyEvent,
+         pushTo,
+         push,
+         remap
        ) where
 
 import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Reader.Class (MonadReader)
+import WildBind.Binding (Binding')
 
 import WildBind.X11.Internal.FrontEnd
   ( X11Front(..)
   )
 import WildBind.X11.Internal.Key
-  ( XKeyEvent
+  ( ToXKeyEvent, KeyEventType(..), XKeyEvent
   )
 import WildBind.X11.Internal.Window
-  ( Window
+  ( Window, ActiveWindow
   )
 
--- | Send a 'XKeyEvent' to the 'Window'.
-sendKeyEventTo :: MonadIO m => X11Front k -> Window -> XKeyEvent -> m ()
+-- | Send a X11 key event to a 'Window'.
+sendKeyEventTo :: (ToXKeyEvent k, MonadIO m)
+               => X11Front i
+               -> Window -- ^ target window
+               -> k -- ^ Key event to send
+               -> m ()
 sendKeyEventTo = undefined
+
+-- | Same as 'sendKeyEventTo', but the target window is obtained from
+-- 'MonadReader'.
+sendKeyEvent :: (ToXKeyEvent k, MonadIO m, MonadReader Window m) => X11Front i -> k -> m ()
+sendKeyEvent = undefined
+
+-- | Send a \"key push\" event to a 'Window', that is, send 'KeyPress'
+-- and 'KeyRelease' events.
+pushTo :: (ToXKeyEvent k, MonadIO m) => X11Front i -> Window -> k -> m ()
+pushTo = undefined
+
+-- | Same as 'pushTo', but the target window is obtained from
+-- 'MonadReader'.
+push :: (ToXKeyEvent k, MonadIO m, MonadReader Window m) => X11Front i -> k -> m ()
+push = undefined
+
+-- | Create a binding that remaps key event \"@from@\" to
+-- \"@to@\".
+--
+-- This binding captures 'KeyPress' and 'KeyRelease' events of
+-- \"@from@\", and sends respective events of \"@to@\" to the active
+-- window.
+remap :: (ToXKeyEvent from, ToXKeyEvent to) => X11Front i -> from -> to -> Binding' bs ActiveWindow XKeyEvent
+remap = undefined
