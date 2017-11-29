4e68c075be04ef072de214dc0faaa2282b97c0f2
Toshio Ito <debug.ito@gmail.com>
2017-11-29 22:46:30 +0900

    EmulateSpec: complete test for sendKeyEventTo.

--- wild-bind-x11/test/WildBind/X11/EmulateSpec.hs
+++ wild-bind-x11/test/WildBind/X11/EmulateSpec.hs
@@ -1,12 +1,16 @@
 module WildBind.X11.EmulateSpec (main,spec) where
 
+import Control.Monad (forM_)
 import qualified Graphics.X11.Xlib as Xlib
 import Test.Hspec
 
+import WildBind (frontNextEvent, FrontEvent(..))
 import WildBind.X11
   ( withX11Front, makeFrontEnd,
-    XMod(..), (.+), release
+    XMod(..), (.+), release,
+    defaultRootWindow
   )
+import WildBind.X11.Emulate (sendKeyEventTo)
 
 import WildBind.X11.TestUtil (checkIfX11Available, withGrabs)
 
@@ -14,12 +18,15 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = checkIfX11Available $ describe "sendKeyEvent" $ do
+spec = checkIfX11Available $ describe "sendKeyEventTo" $ do
   it "should send key event" $ withX11Front $ \x11 -> do
     let front = makeFrontEnd x11
         inputs = [ Alt .+ Super .+ Xlib.xK_r,
                    release Xlib.xK_w
                  ]
+    _ <- frontNextEvent front -- discard the first FEChange
     withGrabs front inputs $ do
-      True `shouldBe` False -- TODO
+      forM_ inputs $ \input -> do
+        sendKeyEventTo x11 (defaultRootWindow x11) input
+        frontNextEvent front `shouldReturn` FEInput input
     
