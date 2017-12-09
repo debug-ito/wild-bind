af34cf5493610956596123576541c0ce02434258
Toshio Ito <debug.ito@gmail.com>
2017-12-09 16:29:38 +0900

    Seq: implement SeqBinding API. failing..

--- wild-bind/src/WildBind/Seq.hs
+++ wild-bind/src/WildBind/Seq.hs
@@ -16,35 +16,45 @@ module WildBind.Seq
 
 import Control.Monad.Trans.State (State)
 import qualified Control.Monad.Trans.State as State
-import Data.Monoid (Monoid(..), (<>))
+import Data.Monoid (Monoid(..), (<>), mconcat)
 
 import WildBind.Binding
-  ( Binding, Binding', binds', whenBack, on, run
+  ( Binding, Binding', binds', whenBack, on, as, run, extend,
+    startFrom
   )
 
-newtype SeqBinding fs i = SeqBinding { unSeqBinding :: [i] -> Binding' [i] fs i }
+newtype SeqBinding fs i = SeqBinding ([i] -> Binding' [i] fs i)
 
-instance Monoid (SeqBinding fs i) where
-  mempty = undefined -- TODO
-  mappend = undefined -- TODO
+instance Ord i => Monoid (SeqBinding fs i) where
+  mempty = SeqBinding $ const mempty
+  mappend (SeqBinding a) (SeqBinding b) =
+    SeqBinding $ \ps -> mappend (a ps) (b ps)
 
+withPrefix :: Ord i => [i] -> SeqBinding fs i -> SeqBinding fs i
+withPrefix [] sb = sb
+withPrefix (p : rest) sb =
+  SeqBinding $ \cur_prefix -> prefixBinding cur_prefix <> nextBinding cur_prefix
+  where
+    prefixBinding cur_prefix = whenBack (== cur_prefix) $ binds' $ do
+      on p `as` "prefix" `run` State.modify (++ [p])
+    nextBinding cur_prefix = case withPrefix rest sb of
+      SeqBinding fb -> fb (cur_prefix ++ [p])
 
-withPrefix :: [i] -> SeqBinding fs i -> SeqBinding fs i
-withPrefix = undefined
-
-toSeq :: Binding fs i -> SeqBinding fs i
-toSeq = undefined
+toSeq :: Eq i => Binding fs i -> SeqBinding fs i
+toSeq b = SeqBinding $ \ps -> whenBack (== ps) $ extend b
 
 fromSeq :: SeqBinding fs i -> Binding fs i
-fromSeq = undefined
+fromSeq (SeqBinding fb) = startFrom [] $ fb []
 
-cancelOn :: i -> SeqBinding fs i
-cancelOn = undefined
+cancelOn :: Ord i => i -> SeqBinding fs i
+cancelOn c = SeqBinding $ const $ binds' $ on c `as` "cancel" `run` State.put []
 
-withCancel :: [i] -> SeqBinding fs i -> SeqBinding fs i
-withCancel = undefined
+withCancel :: Ord i => [i] -> SeqBinding fs i -> SeqBinding fs i
+withCancel cs sb = cancelBindings <> sb
+  where
+    cancelBindings = mconcat $ map cancelOn cs
 
-prefix :: [i] -> [i] -> Binding fs i -> Binding fs i
+prefix :: Ord i => [i] -> [i] -> Binding fs i -> Binding fs i
 prefix rs ps = fromSeq . withCancel rs . withPrefix ps . toSeq
 
 
