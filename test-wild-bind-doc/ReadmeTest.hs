4e266fba823710e1b0a8ec089dceaf1543bd0e8e
Toshio Ito <debug.ito@gmail.com>
2017-12-30 17:08:42 +0900

    ReadmeTest: use Text to load the file content strictly

--- test-wild-bind-doc/ReadmeTest.hs
+++ test-wild-bind-doc/ReadmeTest.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE QuasiQuotes, OverloadedStrings #-}
 module Main (main) where
 
 import Control.Applicative ((<$>))
@@ -6,22 +6,29 @@ import Control.Exception (finally)
 import Control.Monad (forM_)
 import Data.Bool (bool)
 import Data.Foldable (foldl')
-import Data.List (isPrefixOf, reverse, stripPrefix, span)
-import Data.Char (isSpace, toLower)
+import Data.List (isPrefixOf, reverse, span)
+import Data.Char (isSpace)
+import Data.Monoid ((<>))
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 import System.Directory (doesFileExist, removeFile)
 import System.Environment (lookupEnv)
 import System.Exit (ExitCode(ExitSuccess))
-import System.IO (withFile, IOMode(ReadMode), hGetContents, FilePath, writeFile, hPutStrLn, stderr)
+import System.IO (hPutStrLn, stderr)
 import System.Process (waitForProcess, spawnCommand)
 import Test.Hspec
 import Text.RawString.QQ (r)
 
 main :: IO ()
 main = do
+  let sources = [ "../README.md",
+                  "../wild-bind-x11/src/WildBind/X11/Emulate.hs",
+                  "../wild-bind/src/WildBind/Seq.hs"
+                ]
   stack_opts <- getStackOpts
-  withTestCases "../README.md" $ \readme -> do
-    withTestCases "../wild-bind-x11/src/WildBind/X11/Emulate.hs" $ \emu -> do
-      hspec $ specFor stack_opts (readme ++ emu)
+  cases <- concat <$> mapM loadTestCases sources
+  hspec $ specFor stack_opts cases
 
 specFor :: String -> [TestCase] -> Spec
 specFor stack_opts tcs = sequence_ $ map singleSpec tcs
@@ -31,13 +38,11 @@ specFor stack_opts tcs = sequence_ $ map singleSpec tcs
       where
         label = "==== " ++ (tcFileName tc) ++ ": " ++ (show $ tcIndex tc)
 
-withTestCases :: String -> ([TestCase] -> IO a) -> IO a
-withTestCases filename cont = withFile filename ReadMode $ \h -> cont =<< loadFile h
-  where
-    loadFile handle = makeTestCases filename <$> extractExamples <$> hGetContents handle
+loadTestCases :: FilePath -> IO [TestCase]
+loadTestCases filename = makeTestCases filename <$> extractExamples <$> TIO.readFile filename
 
-type CodeBlock = String
-type Prefix = String
+type CodeBlock = Text
+type Prefix = Text
 
 data TestCase = TestCase { tcIndex :: Int,
                            tcPrefix :: CodeBlock,
@@ -49,33 +54,33 @@ data CodeAcc = CodeAcc { caCurrent :: Maybe (CodeBlock, Prefix),
                          caResult :: [CodeBlock]
                        } deriving (Show,Eq,Ord)
 
-extractExamples :: String -> [CodeBlock]
-extractExamples = obtainResult . foldl' f start . lines where
+extractExamples :: Text -> [CodeBlock]
+extractExamples = obtainResult . foldl' f start . T.lines where
   start = CodeAcc Nothing []
   f acc line = case caCurrent acc of
     Nothing | line == "```haskell" -> acc { caCurrent = Just ("", "") }
             | otherwise -> case stripSpacedPrefix "-- > " line of
               Nothing -> acc
               Just (block, prefix) -> if startOfExample block
-                                      then acc { caCurrent = Just $ (block ++ "\n", prefix) }
+                                      then acc { caCurrent = Just $ (block <> "\n", prefix) }
                                       else acc
-    Just (cur, prefix) -> case stripPrefix prefix line of
+    Just (cur, prefix) -> case T.stripPrefix prefix line of
       Nothing -> finish cur acc
       Just line_body | line_body == "```" -> finish cur acc
-                     | "#!" `isPrefixOf` line_body -> acc
-                     | otherwise -> acc { caCurrent = Just $ (cur ++ line_body ++ "\n", prefix) }
+                     | "#!" `T.isPrefixOf` line_body -> acc
+                     | otherwise -> acc { caCurrent = Just $ (cur <> line_body <> "\n", prefix) }
   finish cur acc = CodeAcc { caCurrent = Nothing,
                              caResult = cur : caResult acc
                            }
   obtainResult CodeAcc { caCurrent = mcur, caResult = ret } = reverse $ maybe ret (\(cur, _) -> cur : ret) mcur
-  startOfExample line = map toLower line == "-- example"
+  startOfExample line = T.toLower line == "-- example"
 
-stripSpacedPrefix :: Prefix -> String -> Maybe (String, Prefix)
-stripSpacedPrefix prefix = makeResult . span isSpace
+stripSpacedPrefix :: Prefix -> Text -> Maybe (Text, Prefix)
+stripSpacedPrefix prefix = makeResult . T.span isSpace
   where
     makeResult (spaces, rest) = do
-      body <- stripPrefix prefix rest
-      return (body, spaces ++ prefix)
+      body <- T.stripPrefix prefix rest
+      return (body, spaces <> prefix)
 
 prefixFor :: String -> Int -> CodeBlock
 prefixFor "../README.md" index = readme index
@@ -83,12 +88,12 @@ prefixFor "../README.md" index = readme index
     readme 2 = prefix_basic_process
     readme 3 = prefix_basic_process
     readme 4 = prefix_with_pushKey
-    readme 5 = prefix_with_pushKey ++ [r|
+    readme 5 = prefix_with_pushKey <> [r|
 myBinding = forFirefox
 
 |]
 
-    readme 6 = prefix_basic_process ++ [r|
+    readme 6 = prefix_basic_process <> [r|
 myBinding = binds $ do
   on NumCenter `run` putStrLn "Hello, world!"
 
@@ -127,7 +132,7 @@ main = wildNumPad myBinding
 |]
 
 prefix_with_pushKey :: CodeBlock
-prefix_with_pushKey = prefix_basic_process ++ [r|
+prefix_with_pushKey = prefix_basic_process <> [r|
 
 pushKey key = spawnCommand ("xdotool key " <> key)
 
@@ -152,14 +157,14 @@ getStackOpts = maybe notify_and_default return =<< lookupEnv env_name where
 checkCompile :: String -> TestCase -> Expectation
 checkCompile stack_opts tc = withTempSourceFile tempSourceBase sourceContent doCheck where
   tempSourceBase = "temp_readme_test"
-  sourceContent = tcPrefix tc ++ tcBody tc
+  sourceContent = tcPrefix tc <> tcBody tc
   doCheck = (waitForProcess =<< spawnCommand cmdline) `shouldReturn` ExitSuccess
   cmdline = "stack ghc " ++ stack_opts ++ " " ++ tempSourceBase ++ ".hs"
   
 
-withTempSourceFile :: FilePath -> String -> IO a -> IO a
+withTempSourceFile :: FilePath -> Text -> IO a -> IO a
 withTempSourceFile filebase content act = (createFile >> act) `finally` cleanFiles where
-  createFile = writeFile (filebase ++ ".hs") content
+  createFile = TIO.writeFile (filebase ++ ".hs") content
   cleanFiles = forM_ [".hs", ".o", ".hi", ""] $ \ext -> cleanFile (filebase ++ ext)
   cleanFile filename = bool (return ()) (removeFile filename) =<< doesFileExist filename
 
